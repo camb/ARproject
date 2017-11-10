f6939bd3096bed3b3b4be3e3e5b76073ca2fd78e
camb <cameron.bec@gmail.com>
2017-11-10 02:17:52 -0600

    fixed JPEG error messages

--- videoInputTest.cpp
+++ videoInputTest.cpp
@@ -8,21 +8,25 @@ using namespace cv;
 
 int main (int argc, char** argv) {
     VideoCapture webcam(0);
-    if (!webcam.isOpened()) {
+    if (!webcam.isOpened()){
         CV_Assert("webcam failed to open");
     }
-
-
+    // Set windows resolution
     webcam.set(CAP_PROP_FRAME_HEIGHT, 720);
     webcam.set(CAP_PROP_FRAME_WIDTH, 1280);
+    // Set codec to YUYV to avoid MJPG corrupt JPEG warnings
+    webcam.set(CAP_PROP_FOURCC, CV_FOURCC('Y', 'U', 'Y', 'V'));
 
+    // Continuously capture fames until keystroke
     while (true) {
         // Capture video frame
         Mat cameraFrame;
         webcam.read(cameraFrame);
+
         // Display Result
         imshow("Camera Output", cameraFrame);
-        if (waitKey(30) >= 0) break;
+        if (waitKey(40) >= 0) break;
     }
+
     return 0;
 }
