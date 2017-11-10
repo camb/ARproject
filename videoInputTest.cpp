768ac61e762ae6fc5a744014460b67ceaf9cd95c
camb <cameron.bec@gmail.com>
2017-11-09 20:20:10 -0600

    Resized input resolution

--- videoInputTest.cpp
+++ videoInputTest.cpp
@@ -1,19 +1,28 @@
+// compiled with `pkg-config --libs --cflags opencv`
 #include <opencv2/opencv.hpp>
+#include <iostream>
 
 using namespace std;
 using namespace cv;
 
 
 int main (int argc, char** argv) {
-	VideoCapture stream1(0);
+    VideoCapture webcam(0);
+    if (!webcam.isOpened()) {
+        CV_Assert("webcam failed to open");
+    }
 
-	while (true) {
-		// Capture video frame
-		Mat cameraFrame;
-		stream1.read(cameraFrame);
-		// Display Result
-		imshow("Camera Output", cameraFrame);
-		if (waitKey(30) >= 0) break;
-	}
+
+    webcam.set(CAP_PROP_FRAME_HEIGHT, 720);
+    webcam.set(CAP_PROP_FRAME_WIDTH, 1280);
+
+    while (true) {
+        // Capture video frame
+        Mat cameraFrame;
+        webcam.read(cameraFrame);
+        // Display Result
+        imshow("Camera Output", cameraFrame);
+        if (waitKey(30) >= 0) break;
+    }
     return 0;
 }
