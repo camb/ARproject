decbd26087132cc50c152e3fe263a6da0db1a8d5
camb <cameron.bec@gmail.com>
2017-11-10 03:03:55 -0600

    added chessboard tracking

--- videoInputTest.cpp
+++ videoInputTest.cpp
@@ -1,6 +1,8 @@
 // compiled with `pkg-config --libs --cflags opencv`
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <vector>
+#include <iomanip>
 
 using namespace std;
 using namespace cv;
@@ -17,15 +19,28 @@ int main (int argc, char** argv) {
     // Set codec to YUYV to avoid MJPG corrupt JPEG warnings
     webcam.set(CAP_PROP_FOURCC, CV_FOURCC('Y', 'U', 'Y', 'V'));
 
+
     // Continuously capture fames until keystroke
     while (true) {
         // Capture video frame
-        Mat cameraFrame;
-        webcam.read(cameraFrame);
+        Mat webcamFrame;
+        webcam.read(webcamFrame);
+
+        // Detect 8x8 chessboard pattern, 7x7 internal points
+        Size pattern = Size (7, 7);
+        vector<Point2f> corners;
+        findChessboardCorners(webcamFrame, pattern, corners, CALIB_CB_ADAPTIVE_THRESH + CALIB_CB_NORMALIZE_IMAGE
+        + CALIB_CB_FAST_CHECK);
+
+        Scalar color = Scalar(0, 0, 255);
+        for (auto p : corners) {
+            Rect rect(p.x - 2, p.y - 2, 5, 5);
+            rectangle(webcamFrame, rect, color, 2, LINE_8, 0);
+        }
 
         // Display Result
-        imshow("Camera Output", cameraFrame);
-        if (waitKey(40) >= 0) break;
+        imshow("Camera Output", webcamFrame);
+        if (waitKey(30) >= 0) break;
     }
 
     return 0;
