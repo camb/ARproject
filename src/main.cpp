a1c3e972045c3871310043e4eb7b18920f416d8b
camb <cameron.bec@gmail.com>
2018-01-27 21:24:04 -0600

    refactored for multithreading and clarity

--- src/main.cpp
+++ src/main.cpp
@@ -5,25 +5,24 @@
 #include <iomanip>
 #include <thread>
 
-// TODO: commented out while troubleshooting OpenGL
+// OpenCV includes
 #include <opencv2/opencv.hpp>
 using namespace cv;
 
+// OpenGL includes
 #include <GL/glew.h> // include before gl.h and glfw.h
-
 #include <GLFW/glfw3.h>
 GLFWwindow* window;  // TODO: fix global var
-
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 using namespace glm;
 
-
 #include "shader.hpp"
+using namespace std;
 
+// TODO remove?
 // #include <GL/gl.h>
 // #include <GL/glut.h>
-using namespace std;
 
 
 cv::Mat getWebcamStill() {
@@ -34,76 +33,68 @@ cv::Mat getWebcamStill() {
     webcam.set(CAP_PROP_FRAME_WIDTH, 1280);
     // Set codec to YUYV to avoid MJPG corrupt JPEG warnings
     webcam.set(CAP_PROP_FOURCC, CV_FOURCC('Y', 'U', 'Y', 'V'));
-    Mat webcamStill;
-    webcam.read(webcamStill);
-    // using that in the OpenGl window background
-    // may need to flip texture to work in OpenGL
-    // cv::flip(webcamStill, webcamStill, 0);
-    return webcamStill;
+    Mat webcam_image;
+    webcam.read(webcam_image);
+
+    return webcam_image;
 }
 
 
-GLuint loadBMP_custom(const char * imagepath) {
-    // Data read from the header of the BMP file
-    unsigned char header[54]; // Each BMP file begins by a 54-bytes header
-    unsigned int dataPos;     // Position in the file where the actual data begins
-    unsigned int width, height;
-    unsigned int imageSize;   // = width*height*3
-    // Actual RGB data
-    unsigned char * data;
-
-    // Open the file
-    FILE * file = fopen(imagepath,"rb");
-    if (!file){printf("Image could not be opened\n"); return 0;}
-
-    if ( fread(header, 1, 54, file) != 54 ) { // If not 54 bytes read : problem
-        printf("Not a correct BMP file\n");
-        return false;
-    }
-
-    if ( header[0]!='B' || header[1]!='M' ){
-        printf("Not a correct BMP file\n");
-        return 0;
-    }
-
-    // Read ints from the byte array
-    dataPos    = *(int*)&(header[0x0A]);
-    imageSize  = *(int*)&(header[0x22]);
-    width      = *(int*)&(header[0x12]);
-    height     = *(int*)&(header[0x16]);
-
-    // Some BMP files are misformatted, guess missing information
-    if (imageSize==0)    imageSize=width*height*3; // 3 : one byte for each Red, Green and Blue component
-    if (dataPos==0)      dataPos=54; // The BMP header is done that way
-
-    // Create a buffer
-    data = new unsigned char [imageSize];
-
-    // Read the actual data from the file into the buffer
-    fread(data,1,imageSize,file);
-
-    //Everything is in memory now, the file can be closed
-    fclose(file);
-
-
-    // Create one OpenGL texture
+GLuint cvMatToGLuint(cv::Mat input_mat) {
     GLuint textureID;
+    cv::flip(input_mat, input_mat, 0);
     glGenTextures(1, &textureID);
-
-    // "Bind" the newly created texture : all future texture functions will modify this texture
     glBindTexture(GL_TEXTURE_2D, textureID);
 
-    // Give the image to OpenGL
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, data);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // Set texture clamping method
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+
+
+    glTexImage2D(GL_TEXTURE_2D,     // Type of texture
+                 0,                 // Pyramid level (for mip-mapping) - 0 is the top level
+                 GL_RGB,            // Internal colour format to convert to
+                 input_mat.cols,          // Image width  i.e. 640 for Kinect in standard mode
+                 input_mat.rows,          // Image height i.e. 480 for Kinect in standard mode
+                 0,                 // Border width in pixels (can either be 1 or 0)
+                 GL_BGR, // Input image format (i.e. GL_RGB, GL_RGBA, GL_BGR etc.)
+                 GL_UNSIGNED_BYTE,  // Image data type
+                 input_mat.ptr());        // The actual image data itself
 
     return textureID;
 }
 
 
-int runOpenGL() {
+int runOpenCV() {
+    while (true) {
+        // Capture video frame
+        Mat webcamFrame;
+        webcamFrame = getWebcamStill();
+
+        // Detect 8x8 chessboard pattern, 7x7 internal points
+        Size pattern = Size (7, 7); 
+        vector<Point2f> corners;
+        findChessboardCorners(webcamFrame, pattern, corners, CALIB_CB_ADAPTIVE_THRESH + CALIB_CB_NORMALIZE_IMAGE
+        + CALIB_CB_FAST_CHECK);
+
+        Scalar color = Scalar(0, 0, 255); // pure red
+        for (auto p : corners) {
+            Rect rect(p.x - 2, p.y - 2, 5, 5);
+            rectangle(webcamFrame, rect, color, 2, LINE_8, 0);
+        }
+
+        // Display Result
+        //imshow("Camera Output", webcamFrame);
+        //if (waitKey(30) >= 0) break; // break on keystroke
+    }
+    return 0;
+}
+
+
+int setupGLFWWindow() {
     // Initialise GLFW
     if( !glfwInit() )
     {
@@ -144,6 +135,69 @@ int runOpenGL() {
     glEnable(GL_DEPTH_TEST);
     // Accept fragment if it closer to the camera than the former one
     glDepthFunc(GL_LESS);
+    return 0;
+}
+
+
+void renderOpenGLFrame(GLuint& programID, GLuint& MatrixID,
+                       GLuint& Texture, GLuint& TextureID,
+                       GLuint& vertexbuffer, GLuint& uvbuffer,
+                       glm::mat4& MVP) {
+    // Clear the screen
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // Use our shader
+    glUseProgram(programID);
+
+    // Send our transformation to the currently bound shader, 
+    // in the "MVP" uniform
+    glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+
+    // Bind our texture in Texture Unit 0
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, Texture);
+    // Set our "myTextureSampler" sampler to use Texture Unit 0
+    glUniform1i(TextureID, 0);
+
+
+    // 1rst attribute buffer : vertices
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glVertexAttribPointer(
+        0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+        3,                  // size
+        GL_FLOAT,           // type
+        GL_FALSE,           // normalized?
+        0,                  // stride
+        (void*)0            // array buffer offset
+    );
+
+    // 2nd attribute buffer : UVs
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+    glVertexAttribPointer(
+        1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+        2,                                // size : U+V => 2
+        GL_FLOAT,                         // type
+        GL_FALSE,                         // normalized?
+        0,                                // stride
+        (void*)0                          // array buffer offset
+    );
+
+    // Draw the triangles
+    glDrawArrays(GL_TRIANGLES, 0, 2*3);
+
+    glDisableVertexAttribArray(0);
+    glDisableVertexAttribArray(1);
+
+    // Swap buffers
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+}
+
+
+int main (int argc, char** argv) {
+    setupGLFWWindow();
 
     GLuint VertexArrayID;
     glGenVertexArrays(1, &VertexArrayID);
@@ -160,9 +214,9 @@ int runOpenGL() {
     glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
     // Camera matrix
     glm::mat4 View       = glm::lookAt(
-                                glm::vec3(0,0,3), // Camera is at (0,0,3), in World Space
-                                glm::vec3(0,0,0), // and looks at the origin
-                                glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+                            glm::vec3(0,0,3), // Camera is at (0,0,3), in World Space
+                            glm::vec3(0,0,0), // and looks at the origin
+                            glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
                            );
     // Model matrix : an identity matrix (model will be at the origin)
     glm::mat4 Model      = glm::mat4(1.0f);
@@ -170,10 +224,8 @@ int runOpenGL() {
     glm::mat4 MVP        = Projection * View * Model; // Remember, matrix multiplication is the other way around
 
 
-
-
-    // Load the texture file
-    GLuint Texture = loadBMP_custom("grid.bmp");
+    // Take webcam still and load background
+    GLuint Texture = cvMatToGLuint(getWebcamStill());
     
     // Get a handle for our "myTextureSampler" uniform
     GLuint TextureID  = glGetUniformLocation(programID, "myTextureSampler");
@@ -189,13 +241,14 @@ int runOpenGL() {
     };
 
     // Two UV coordinatesfor each vertex
+    // TODO fix slight texture overlapping issue
     static const GLfloat g_uv_buffer_data[] = {
-    1.0f, 0.0f,
-    1.0f, 1.0f,
-    0.0f, 0.0f,
-    0.0f, 0.0f,
-    1.0f, 1.0f,
-    0.0f, 1.0f,
+        0.0f, 1.0f,
+        1.0f, 1.0f,
+        0.0f, 0.0f,
+        0.0f, 0.0f,
+        1.0f, 1.0f,
+        1.0f, 0.0f,
     };
 
     GLuint uvbuffer;
@@ -208,61 +261,28 @@ int runOpenGL() {
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
+
+    // capture a webcam frame
+    // thread 1 tracks newest webcam frame
+    // thread 2 renders previous webcam frame with overlayed 3D
+    // join threads and display previous image, repeat with newest frame
+    // capture and solve works 1 step ahead
     do{
-        // Clear the screen
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        renderOpenGLFrame(programID, MatrixID,
+                          Texture,TextureID,
+                          vertexbuffer, uvbuffer,
+                          MVP);
+        //thread first (renderOpenGLFrame);
+        //thread second(trackLatestFrame);
 
-        // Use our shader
-        glUseProgram(programID);
-
-        // Send our transformation to the currently bound shader, 
-        // in the "MVP" uniform
-        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
-
-        // Bind our texture in Texture Unit 0
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, Texture);
-        // Set our "myTextureSampler" sampler to use Texture Unit 0
-        glUniform1i(TextureID, 0);
-
-
-        // 1rst attribute buffer : vertices
-        glEnableVertexAttribArray(0);
-        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-        glVertexAttribPointer(
-            0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-            3,                  // size
-            GL_FLOAT,           // type
-            GL_FALSE,           // normalized?
-            0,                  // stride
-            (void*)0            // array buffer offset
-        );
-
-        // 2nd attribute buffer : UVs
-        glEnableVertexAttribArray(1);
-        glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
-        glVertexAttribPointer(
-            1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
-            2,                                // size : U+V => 2
-            GL_FLOAT,                         // type
-            GL_FALSE,                         // normalized?
-            0,                                // stride
-            (void*)0                          // array buffer offset
-        );
-
-        // Draw the triangles
-        glDrawArrays(GL_TRIANGLES, 0, 2*3);
-
-        glDisableVertexAttribArray(0);
-        glDisableVertexAttribArray(1);
-
-        // Swap buffers
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+        // synchronize threads
+        //first.join();
+        //second.join();
 
     } // Check if the ESC key was pressed or the window was closed
     while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
            glfwWindowShouldClose(window) == 0 );
+
 
     // Cleanup VBO
     glDeleteBuffers(1, &vertexbuffer);
@@ -273,53 +293,5 @@ int runOpenGL() {
 
     // Close OpenGL window and terminate GLFW
     glfwTerminate();
-    return 0;
-}
-
-
-int runOpenCV() {
-    VideoCapture webcam(0);
-    if (!webcam.isOpened()){
-        CV_Assert("webcam failed to open");
-    }
-    // Set windows resolution
-    webcam.set(CAP_PROP_FRAME_HEIGHT, 720);
-    webcam.set(CAP_PROP_FRAME_WIDTH, 1280);
-    // Set codec to YUYV to avoid MJPG corrupt JPEG warnings
-    webcam.set(CAP_PROP_FOURCC, CV_FOURCC('Y', 'U', 'Y', 'V'));
-
-    while (true) {
-        // Capture video frame
-        Mat webcamFrame;
-        webcam.read(webcamFrame);
-
-        // Detect 8x8 chessboard pattern, 7x7 internal points
-        Size pattern = Size (7, 7); 
-        vector<Point2f> corners;
-        findChessboardCorners(webcamFrame, pattern, corners, CALIB_CB_ADAPTIVE_THRESH + CALIB_CB_NORMALIZE_IMAGE
-        + CALIB_CB_FAST_CHECK);
-
-        Scalar color = Scalar(0, 0, 255); // pure red
-        for (auto p : corners) {
-            Rect rect(p.x - 2, p.y - 2, 5, 5);
-            rectangle(webcamFrame, rect, color, 2, LINE_8, 0);
-        }
-
-        // Display Result
-        imshow("Camera Output", webcamFrame);
-        if (waitKey(30) >= 0) break; // break on keystroke
-    }
-    return 0;
-}
-
-
-int main (int argc, char** argv) {
-    thread first (runOpenGL);
-    //thread second (runOpenCV);
-
-    // synchronize threads
-    first.join();
-    //second.join();
-
     return 0;
 }
